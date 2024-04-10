f185d1f9acf4d7489189090560b3d921d36af410
billi <wayne9303@naver.com>
2024-04-10 16:08:15 +0900

    스타일 추가 > Loader

--- src/stories/Loader/PageLoader.jsx
+++ src/stories/Loader/PageLoader.jsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
+import classNames from "classnames/bind";
 
-function Loader({ isLoading }) {
+import styles from "./PageLoader.module.scss";
+
+const cx = classNames.bind(styles);
+
+function PageLoader() {
+  const [isLoading, setIsLoading] = useState(false);
   const [showLoadingMessage, setShowLoadingMessage] = useState(false);
   const [showCompleteMessage, setShowCompleteMessage] = useState(false);
 
@@ -8,7 +14,14 @@ function Loader({ isLoading }) {
     if (isLoading) {
       setShowLoadingMessage(true);
 
-      return;
+      // 3초 로딩
+      const showLoadingTimer = setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
+
+      return () => {
+        clearTimeout(showLoadingTimer);
+      };
     }
 
     // 로딩 완료되면 '로딩 중' 제거
@@ -29,20 +42,29 @@ function Loader({ isLoading }) {
 
   return (
     <>
-      {!isLoading && <main>로딩 완료 후 렌더링 되는 페이지</main>}
+      <button
+        type="button"
+        className={cx("button")}
+        onClick={() => setIsLoading(true)}
+      >
+        로딩(3초) 시작
+      </button>
+
+      {!isLoading && (
+        <main className={cx("page")}>로딩 완료 후 렌더링 되는 페이지</main>
+      )}
 
       <div
         // 1. 로딩 상태 메시지를 즉각적으로 안내하도록 ARIA 속성 추가
         aria-live="assertive"
         // 2. 로딩 상태 메시지 전체를 안내하도록 ARIA 속성 추가
         aria-atomic="true"
+        className={cx("loader")}
       >
         {showLoadingMessage && (
           <>
             {/* 로딩을 시각적으로 제공하는 UI */}
-            {/* TODO: 빌리 */}
-            {/* <Loading /> */}
-            로딩
+            <div className={cx("loading-ui")} />
             {/* 3. 로딩 중인 경우 '로딩 중' 메시지 안내 */}
             <span className="visually-hidden">로딩 중</span>
           </>
@@ -57,4 +79,4 @@ function Loader({ isLoading }) {
   );
 }
 
-export default Loader;
+export default PageLoader;
---
+++ src/stories/Loader/PageLoader.stories.js
@@ -0,0 +1,10 @@
+import PageLoader from "./PageLoader";
+
+export default {
+  title: "Part1/Loader",
+  component: PageLoader,
+};
+
+export const BasicPageLoader = {
+  name: "PageLoader",
+};
